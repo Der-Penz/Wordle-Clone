1888988149a2bb91150d438767cf0690f0ac9c56
DerPenz <mike2407@t-online.de>
2022-03-27 20:50:04 +0200

    📉 added chart for guess distribution -layout fixes -added random Wordle - added info messages -bug fixes

--- script.js
+++ script.js
@@ -21,16 +21,21 @@ $(document).ready(function () {
         $("body").removeClass("darkmode");
         $("body").addClass("lightmode");
     }
+
+    
 });
 
 const WORD_LENGTH = 5
 const NUMBER_OF_GUESSES = 6;
 const gridLetters = $(".letter");
 const keyboard = $(".key");
-const TO_GUESS = TARGET_WORDS[getDaily()].toUpperCase();
-var gameEnded = false;
+var TO_GUESS = TARGET_WORDS[getDaily()].toUpperCase();
+var gameEnded = true;
 var pauseInput = false;
 var indexSetted = true;
+var gameMode = "daily";
+var ctx = document.getElementById('guessChart');
+var myChart;
 
 var fullClear = false;
 var guessIndex = 0;
@@ -44,10 +49,41 @@ var grid = [
             ["", "", "", "", ""],
            ]
 
+function checkForDaily(){
+    if(!document.cookie.includes("playedDaily=true")){
+        gameEnded = false;
+        return;
+    }
+    showToast('<div style="text-align: center">Already played daily Wordle <br> press to play a random Wordle?</div>',
+             "5000",
+              "info",
+              () => playRandom());
+    gameEnded = true;
+
+    let cookies = document.cookie.replace("playedDaily=true; ", "").split("; ");
+
+    cookies.forEach((row, i) => {
+        let word = row.split("=")[1];
+        for (let j = 0; j < word.length; j++) {
+            guessLetter(word.charAt(j));
+        }
+        guessIndex++;
+        letterIndex = 0;
+    });
+    
+    console.table(grid);
+}
+
+setTimeout(() => {
+    checkForDaily();
+    initChart();
+}, 1000);
+
+
 /**
  * returns the daily wordle
  */
-function getDaily() {
+function getDaily(){
     let days = Math.floor(Date.now() / 1000 / 60 / 60 / 24);
     return (days * days) % TARGET_WORDS.length;
 }
@@ -141,16 +177,59 @@ function colorKey(whichLetter, color){
 }
 
 /**
+ * shows a message on the webside
+ */
+function showToast(message, duration = "1500", type = "error", onClick = null){
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-center",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "200",
+        "hideDuration": "200",
+        "timeOut": duration,
+        "extendedTimeOut": "500",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr.options.onclick = onClick;
+
+    if(type == "error")
+        toastr.error(message);
+    else if(type == "info")
+        toastr.info(message);
+    else if(type = "success")        
+        toastr.success(message);
+    else if(type = "info")        
+        toastr.info(message);
+}
+
+/**
  * handles the logic for checking whats right and if the player lost or won 
  */
 function submitGuess(){
     if(pauseInput)
         return;
     pauseInput = true;
-    let guessedWord = grid[guessIndex].reduce((word, letter) => word += letter);
 
-    if(guessedWord.length < WORD_LENGTH || !isValidWord(guessedWord)){
+    let guessedWord = grid[guessIndex].reduce((word, letter) => word += letter);
+    console.log(guessedWord);
+    if(guessedWord.length < WORD_LENGTH){
         shakeRow();
+        showToast("Word to short");
+        pauseInput = false;
+        return;
+    }
+
+    if(!isValidWord(guessedWord)){
+        shakeRow();
+        showToast("No valid input");
         pauseInput = false;
         return;
     }
@@ -161,7 +240,7 @@ function submitGuess(){
     })
 
     let letterColorPair = [];
-
+    
     //first check the green positions and save the remaining letters
     grid[guessIndex].forEach((letter,i) =>{
         if(letter == TO_GUESS.charAt(i)){
@@ -188,6 +267,7 @@ function submitGuess(){
             gameEnded = true;
             jumpRow();
             saveGame("1");
+            showToast('<div style="text-align: center">You won GG</div>', "3000", "success");
             return;
         }
 
@@ -195,9 +275,9 @@ function submitGuess(){
         letterIndex = 0;
     
         if(guessIndex == NUMBER_OF_GUESSES){
-            alert(`You lost, ${TO_GUESS} was the correct word`);
             gameEnded = true;
             saveGame("0");
+            showToast(`<div style="text-align: center">You lost: <p style="font-weight: 900">${TO_GUESS}</p> was the correct word</div>`, "5000");
         }
     }, wait);
 }
@@ -238,10 +318,41 @@ function isValidWord(word){
     return VALID_WORDS.includes(word.toLowerCase());
 }
 
+function playRandom(){
+    gameMode = "random";
+    gameEnded = false;
+    TO_GUESS = TARGET_WORDS[(Math.floor(Math.random() * TARGET_WORDS.length)) % TARGET_WORDS.length].toUpperCase();
+    
+    grid.forEach((e, i) => {
+        e.forEach((el, j) => {
+            e[j] = "";
+        });
+    });
+
+    $(keyboard).each(function (index, element) {
+        $(element).attr("taged", "false").css({backgroundColor: "var(--bg-secondary)", color: "var(--font-primary)"});
+    });
+
+    $(gridLetters).each(function (index, element) {
+        // element == this
+        $(element).attr("data", "").css({
+            backgroundColor: "var(--bg-primary)",
+            borderColor: "var(--bg-secondary)",
+            color: "var(--font-primary)"});
+    });
+
+    guessIndex = 0;
+    letterIndex = 0;
+
+}
+
 /**
  * saves data from the game 
  */
 function saveGame(isWin){
+    if(gameMode == "random")
+        return;
+
     localStorage.setItem("played", localStorage.getItem("played") != null ? parseInt(localStorage.getItem("played")) + 1 : 1);
     
     if(isWin == "1"){
@@ -270,10 +381,15 @@ function saveGame(isWin){
         localStorage.setItem("streak", 0);
     }
 
-    grid.forEach((element, i) => {
-        let word = element.join("");        
+    var date = new Date();
+    date.setHours(23, 59, 59, 999);
+    var expires = "expires=" + date.toUTCString();
 
-        localStorage.setItem("row" + i, word);
+    document.cookie = "playedDaily=true; " + expires;
+    grid.forEach((element, i) => {
+        let word = element.join("");      
+
+        document.cookie = `row${i}=${word}; ${expires} `;
     });
 }   
 
@@ -289,7 +405,8 @@ function toggleStats(){
     let winPercentage = Number(parseInt(localStorage.getItem("wins")) / parseInt(localStorage.getItem("played")));
     if(isNaN(winPercentage))
         winPercentage = 0;
-    $(".value[stats-type=win]").text(winPercentage.toFixed(2) * 100);
+
+    $(".value[stats-type=win]").text((winPercentage * 100).toFixed(1));
 
     let streak = localStorage.getItem("streak");
     if(streak == null)
@@ -302,6 +419,51 @@ function toggleStats(){
     $(".value[stats-type=maxStreak]").text(maxStreak);
 
     $(".stats-popup").toggleClass("hide");
+}
+
+function initChart(){
+   
+    let guesses = [];
+
+    for (let i = 1; i < 7; i++) {
+        guesses.push(localStorage.getItem("guessedAt" + i) == null ? 0 : localStorage.getItem("guessedAt" + i));  
+    }
+
+    myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['1', '2', '3', '4', '5', '6'],
+            datasets: [{
+                label: '',
+                data: [guesses[0], guesses[1], guesses[2], guesses[3], guesses[4], guesses[5]],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            events: ['none']
+        }
+    });
 }
 
 /**
@@ -383,10 +545,8 @@ $(".letter").on("click", function (e) {
     if(gameEnded)
         return;
     let i = $(e.target).attr("index");
-    if(!(i < guessIndex * WORD_LENGTH + WORD_LENGTH && i >= guessIndex * WORD_LENGTH)){
-        console.log(i);
+    if(!(i < guessIndex * WORD_LENGTH + WORD_LENGTH && i >= guessIndex * WORD_LENGTH))
         return;
-    }
     indexSetted = true;
     letterIndex = i % WORD_LENGTH;
 });
@@ -402,7 +562,6 @@ $("html").keyup(function (e) {
 });
 
 $(".icon").on("click", function () {
-
     let iconType = $(this).attr("iconType");
 
     switch (iconType) {
